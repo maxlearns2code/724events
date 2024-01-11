01d5ebe927def0887b9a43b20a0767f21299003d
Maxime <maxdevvynter@gmail.com>
2024-01-11 14:31:19 +0100

    fix anchor element

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -15,7 +15,6 @@ import { useData } from "../../contexts/DataContext";
 const Page = () => {
   const { last } = useData();
   if (!last) return null;
-  console.log(last);
   return (
     <>
       <header>
@@ -26,7 +25,7 @@ const Page = () => {
           <Slider />
         </section>
         <section className="ServicesContainer">
-          <h2 className="Title">Nos services</h2>
+          <h2 className="Title"  id="nos-services">Nos services</h2>
           <p>Nous organisons des événements sur mesure partout dans le monde</p>
           <div className="ListContainer">
             <ServiceCard imageSrc="/images/priscilla-du-preez-Q7wGvnbuwj0-unsplash1.png">
@@ -55,11 +54,11 @@ const Page = () => {
           </div>
         </section>
         <section className="EventsContainer">
-          <h2 className="Title">Nos réalisations</h2>
+          <h2 className="Title" id="nos-realisations">Nos réalisations</h2>
           <EventList />
         </section>
         <section className="PeoplesContainer">
-          <h2 className="Title">Notre équipe</h2>
+          <h2 className="Title" id="notre-equipe">Notre équipe</h2>
           <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
           <div className="ListContainer">
             <PeopleCard
